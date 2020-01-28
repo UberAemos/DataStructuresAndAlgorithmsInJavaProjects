e8b72aad10eb39f8f31b09a3cdc7a1dbd1f96bf3
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-01-28 11:39:03 +0300

    Adds addScore method to SinglyLinkedList ScoreBoard implementation.

--- src/projects/fundamental/LinkedScoreBoard.java
+++ src/projects/fundamental/LinkedScoreBoard.java
@@ -1,7 +1,32 @@
 package projects.fundamental;
 
+import base.fundamental.GameEntry;
+import base.fundamental.SinglyLinkedList;
+
 /**
  * Maintains the top ten scores for a game application using a singly linked list.
  */
 public class LinkedScoreBoard {
+    final private static int capacity = 10;                                    // total capacity of this scoreboard
+    private SinglyLinkedList<GameEntry> board = new SinglyLinkedList<>();      // list of game entries (names & scores)
+
+    /**
+     * Attempt to add a new score to the collection (if it is high enough)
+     */
+    public void add(GameEntry e) {
+        int newScore = e.getScore();
+        // is the new entry e really a high score?
+        if (board.size() < capacity || newScore > board.first().getScore()) {
+            // if smallest score so far add it to the front
+            if (board.first().getScore() > e.getScore() || board.isEmpty()) board.addFirst(e);
+                // else insert it at the top of closest smaller element
+            else {
+                SinglyLinkedList.Node<GameEntry> walk = board.getHead();
+                if (walk.getNext().getElement().getScore() > e.getScore())
+                    walk.setNext(new SinglyLinkedList.Node<>(e, walk.getNext()));
+            }
+            // if capacity is exceeded remove the first element
+            if (board.size() > capacity) board.removeFirst();
+        }
+    }
 }
