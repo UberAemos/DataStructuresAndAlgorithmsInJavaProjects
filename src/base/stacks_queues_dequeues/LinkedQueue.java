51aa1a904eaf0b3dfa662d9e21f070cc751e0af7
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-01-30 15:28:48 +0300

    Adds book implementation of LinkedQueue.

---
+++ src/base/stacks_queues_dequeues/LinkedQueue.java
@@ -0,0 +1,30 @@
+package base.stacks_queues_dequeues;
+
+import base.fundamental.SinglyLinkedList;
+
+/**
+ * Realization of a FIFO queue as an adaptation of a SinglyLinkedList.
+ */
+public class LinkedQueue<E> implements Queue<E> {
+    private SinglyLinkedList<E> list = new SinglyLinkedList<>();    // an empty list
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public void enqueue(E element) {
+        list.addLast(element);
+    }
+
+    public E first() {
+        return list.first();
+    }
+
+    public E dequeue() {
+        return list.removeFirst();
+    }
+}
