4cb811914cb03f1bf190029ce45100861acdcbdc
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-02-06 09:31:00 +0300

    Adds unit tests for CapitalGainEvaluator.

--- src/projects/stacks_queues_dequeues/CapitalGainEvaluatorTest.java
+++ src/projects/stacks_queues_dequeues/CapitalGainEvaluatorTest.java
@@ -2,6 +2,7 @@ package projects.stacks_queues_dequeues;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import projects.stacks_queues_dequeues.exception.InssufficientShareException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -12,6 +13,14 @@ class CapitalGainEvaluatorTest {
     private String buyTransaction2 = "buy 20 share(s) at $24 each";
     private String buyTransaction3 = "buy 200 share(s) at $36 each";
     private String sellTransaction1 = "sell 150 share(s) at $30 each";
+    private String invalidTransaction = "feed one-fifty gazelle(s) at $30 each";
+
+    private CapitalGainEvaluator emptyGainEvaluator;
+
+    @BeforeEach
+    void init() {
+        emptyGainEvaluator = new CapitalGainEvaluator();
+    }
 
     @Test
     void whenValidBuyTransactionIsGivenToShareFactory_shareIsCreated() {
@@ -21,13 +30,60 @@ class CapitalGainEvaluatorTest {
     }
 
     @Test
-    void whenInvalidBuyTransactionIsGivenToShareFactory_exceptionIsThrown() {
-        assertThrows(IllegalArgumentException.class, () -> CapitalGainEvaluator.Share.fromTransaction(sellTransaction1));
+    void whenInvalidTransactionIsGiven_throwsException() {
+        assertThrows(IllegalArgumentException.class, () -> emptyGainEvaluator.transact(invalidTransaction));
     }
 
-    @BeforeEach
-    void init() {
-
+    @Test
+    void whenBuyTransactionIsGivenToEmptyEvaluator_returnsSharesTotalValueAsCapitalLoss() {
+        assertEquals(0, emptyGainEvaluator.transact(buyTransaction1));
+        assertEquals("CapitalGainEvaluator{" +
+                        "shareQueue=[Share{count=100, value=20}]" +
+                        ", capitalGain=0" +
+                        ", totalShareAmount=100" +
+                        '}',
+                emptyGainEvaluator.toString());
     }
 
+    @Test
+    void whenBuyTransactionIsGivenToSingleShareEvaluator_returnsTotalValueAsCapitalLoss() {
+        emptyGainEvaluator.transact(buyTransaction1);
+        assertEquals(0, emptyGainEvaluator.transact(buyTransaction2));
+        assertEquals("CapitalGainEvaluator{" +
+                        "shareQueue=[Share{count=20, value=24}, Share{count=100, value=20}]" +
+                        ", capitalGain=0" +
+                        ", totalShareAmount=120" +
+                        '}',
+                emptyGainEvaluator.toString());
+    }
+
+    @Test
+    void whenSellTransactionIsGivenWithInsufficientShareAmount_exceptionIsThrown() {
+        emptyGainEvaluator.transact(buyTransaction1);
+        assertThrows(InssufficientShareException.class, () -> emptyGainEvaluator.transact(sellTransaction1));
+    }
+
+    @Test
+    void whenSellTransaction1IsGivenAfterBuyTransaction3_returnsCapitalLoss() {
+        emptyGainEvaluator.transact(buyTransaction3);
+        assertEquals(-900, emptyGainEvaluator.transact(buyTransaction1));
+        assertEquals("CapitalGainEvaluator{" +
+                        "shareQueue=[Share{count=50, value=36}]" +
+                        ", capitalGain=-900" +
+                        ", totalShareAmount=50" +
+                        '}',
+                emptyGainEvaluator.toString());
+    }
+
+    @Test
+    void whenSellTransaction1IsGivenAfterBuyTransaction123_returnsCapitalGain() {
+        emptyGainEvaluator.transact(buyTransaction3);
+        assertEquals(940, emptyGainEvaluator.transact(buyTransaction1));
+        assertEquals("CapitalGainEvaluator{" +
+                        "shareQueue=[Share{count=170, value=36}]" +
+                        ", capitalGain=940" +
+                        ", totalShareAmount=170" +
+                        '}',
+                emptyGainEvaluator.toString());
+    }
 }
