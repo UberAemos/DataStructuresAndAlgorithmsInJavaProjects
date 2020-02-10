65ae63587de53cb21ebab4220a4c37c1d89635e2
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-02-10 14:17:34 +0300

    Adds unit tested toString, bluePush, and redPush methods.

--- src/projects/stacks_queues_dequeues/double_stack/ArrayDoubleStack.java
+++ src/projects/stacks_queues_dequeues/double_stack/ArrayDoubleStack.java
@@ -1,4 +1,107 @@
 package projects.stacks_queues_dequeues.double_stack;
 
-public class ArrayDoubleStack {
+public class ArrayDoubleStack<E> implements DoubleStack<E> {
+    public static final int CAPACITY = 1000;    // default array capacity
+    private E[] data;                           // generic array used for storage
+    private int tBlue = -1;                     // index of top element in blue stack
+    private int tRed = (CAPACITY / 2) - 1;      // index of top element in red stack
+
+    public ArrayDoubleStack() {
+        this(CAPACITY);
+    }     // constructs stack with default capacity
+
+    public ArrayDoubleStack(int capacity) {           // constructs stack with given capacity
+        data = (E[]) new Object[capacity];      // safe cast; compiler may give warning
+    }
+
+    @Override
+    public String toString() {
+        String blueList = "[";
+        String redList = "[";
+
+        if (!isBlueEmpty()) {
+            for (int i = 0; i < tBlue + 1; i++) {
+                blueList = blueList.concat(data[i] + ", ");
+            }
+            blueList = blueList.substring(0, blueList.length() - 2);
+        }
+        if (!isRedEmpty()) {
+            for (int j = CAPACITY / 2; j < tRed + 1; j++) {
+                redList = redList.concat(data[j] + ", ");
+            }
+            redList = redList.substring(0, redList.length() - 2);
+        }
+        blueList = blueList.concat("]");
+        redList = redList.concat("]");
+
+        return "ArrayDoubleStack{" +
+                "blueStack=" + blueList +
+                ", redStack=" + redList +
+                "}";
+    }
+
+    @Override
+    public int redSize() {
+        return tRed + 2;
+    }
+
+    @Override
+    public int blueSize() {
+        return tBlue + 2;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return tBlue + tRed == (CAPACITY / 2) - 2;
+    }
+
+    @Override
+    public boolean isBlueEmpty() {
+        return tBlue == -1;
+    }
+
+    @Override
+    public boolean isRedEmpty() {
+        return tRed == (CAPACITY / 2) - 1;
+    }
+
+    /**
+     * Adds element e to the blue stack
+     *
+     * @param e the element to be inserted
+     */
+    public void bluePush(E e) {
+        if (tBlue == CAPACITY / 2 - 1) throw new IllegalStateException("Blue stack is full");
+        data[++tBlue] = e;
+    }
+
+    @Override
+    public E blueTop() {
+        return null;
+    }
+
+    @Override
+    public E bluePop() {
+        return null;
+    }
+
+    /**
+     * Adds element e to the red stack
+     *
+     * @param e the element to be inserted
+     */
+    public void redPush(E e) {
+        if (tRed == CAPACITY - 1) throw new IllegalStateException("Red stack is full");
+        data[++tRed] = e;
+    }
+
+    @Override
+    public E redTop() {
+        return null;
+    }
+
+    @Override
+    public E redPop() {
+        return null;
+    }
 }
--- src/projects/stacks_queues_dequeues/double_stack/ArrayDoubleStackTest.java
+++ src/projects/stacks_queues_dequeues/double_stack/ArrayDoubleStackTest.java
@@ -4,48 +4,72 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ArrayDoubleStackTest {
 
-    ArrayDoubleStack emptyDoubleStack;
-    ArrayDoubleStack exampleDoubleStack;
+    ArrayDoubleStack<Integer> emptyDoubleStack;
+    ArrayDoubleStack<Integer> exampleDoubleStack;
+    ArrayDoubleStack<Integer> fullStack;
 
     @BeforeEach
     void init() {
-        emptyDoubleStack = new ArrayDoubleStack();
-        exampleDoubleStack = new ArrayDoubleStack();
+        emptyDoubleStack = new ArrayDoubleStack<>();
+
+        exampleDoubleStack = new ArrayDoubleStack<>();
+        exampleDoubleStack.bluePush(1);
+        exampleDoubleStack.bluePush(2);
+        exampleDoubleStack.bluePush(3);
+        exampleDoubleStack.redPush(500);
+        exampleDoubleStack.redPush(501);
+
+        fullStack = new ArrayDoubleStack<>();
+        for (int i = 0; i < 500; i++) fullStack.bluePush(i);
+        for (int i = 500; i < 1000; i++) fullStack.redPush(i);
     }
 
     @Test
     void whenEmptyStackIsPrinted_itPrintsInCorrectFormat() {
         assertEquals("ArrayDoubleStack{" +
-                        "blue=[], " +
-                        "red=[]}",
+                        "blueStack=[], " +
+                        "redStack=[]}",
                 emptyDoubleStack.toString());
     }
 
     @Test
-    void redSize() {
+    void whenElementIsPushedToEmptyStacksBlueStack_thenPrintsElementWithinBlueStack() {
+        emptyDoubleStack.bluePush(1);
+        assertEquals("ArrayDoubleStack{" +
+                        "blueStack=[1], " +
+                        "redStack=[]}",
+                emptyDoubleStack.toString());
     }
 
     @Test
-    void blueSize() {
+    void whenElementIsPushedToEmptyStacksRedStack_thenPrintsElementWithinRedStack() {
+        emptyDoubleStack.redPush(1);
+        assertEquals("ArrayDoubleStack{" +
+                        "blueStack=[], " +
+                        "redStack=[1]}",
+                emptyDoubleStack.toString());
     }
 
     @Test
-    void isEmpty() {
+    void whenRedAndBlueNonEmpty_thenPrintsElementWithinRedAndBlueStack() {
+        assertEquals("ArrayDoubleStack{" +
+                        "blueStack=[1, 2, 3], " +
+                        "redStack=[500, 501]}",
+                exampleDoubleStack.toString());
     }
 
     @Test
-    void isBlueEmpty() {
+    void whenElementIsPushedToFullStackBlue_thenThrowsIllegalStateException() {
+        assertThrows(IllegalStateException.class, () -> fullStack.bluePush(1));
     }
 
     @Test
-    void isRedEmpty() {
-    }
-
-    @Test
-    void bluePush() {
+    void whenElementIsPushedToFullStackRed_thenThrowsIllegalStateException() {
+        assertThrows(IllegalStateException.class, () -> fullStack.redPush(1));
     }
 
     @Test
