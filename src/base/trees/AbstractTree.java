4cb2235e9d7c5dc8c6877d8ec3ade4c6ba9d1eb9
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-02-11 15:49:25 +0300

    Adds book implementations for AbstractBinaryTree, AbstractTree, BinaryTree, Position, and Tree.

---
+++ src/base/trees/AbstractTree.java
@@ -0,0 +1,15 @@
+package base.trees;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+    public boolean isInternal(Position<E> p) {
+        return numChildren(p) > 0;
+    }
+
+    public boolean isExternal(Position<E> p) {
+        return numChildren(p) == 0;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+}
