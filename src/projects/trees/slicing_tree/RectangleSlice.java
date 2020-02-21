4cc68c0049d9f58de939c684855013eb18030cd3
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-02-21 14:37:52 +0300

    Adds RectangleSlice implementation.

--- src/projects/trees/slicing_tree/RectangleSlice.java
+++ src/projects/trees/slicing_tree/RectangleSlice.java
@@ -1,4 +1,24 @@
 package projects.trees.slicing_tree;
 
-public class RectangleSlice {
+/**
+ * Rectangle slice with minimum width and height.
+ */
+public class RectangleSlice extends Slice {
+
+    private int width;
+    private int height;
+
+    public RectangleSlice(String element, int width, int height) {
+        super(element);
+        this.width = width;
+        this.height = height;
+    }
+
+    public int width() {
+        return width;
+    }
+
+    public int height() {
+        return height;
+    }
 }
