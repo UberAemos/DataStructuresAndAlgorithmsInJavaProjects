24ba56bc90a61243803004c88ac142c875d2a157
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-02-21 14:21:27 +0300

    Adds blank SlicingTree class.

---
+++ src/projects/trees/slicing_tree/SlicingTree.java
@@ -0,0 +1,11 @@
+package projects.trees.slicing_tree;
+
+/**
+ * A slicing floor plan divides a rectangle with horizontal and vertical sides using horizontal and vertical cuts.
+ * A slicing floor plan can be represented by a proper binary tree, called a slicing tree, whose internal nodes
+ * represent the cuts, and whose external nodes represent the basic rectangles into which the floor plan is decomposed
+ * by the cuts.
+ */
+public class SlicingTree {
+
+}
