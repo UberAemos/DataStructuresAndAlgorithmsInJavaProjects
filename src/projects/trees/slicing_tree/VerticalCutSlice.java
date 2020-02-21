f2fa9f0980d8722577ffa660dcfb2740465f742c
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-02-21 15:02:02 +0300

    Adds HorizontalCutSlice implementation.

---
+++ src/projects/trees/slicing_tree/VerticalCutSlice.java
@@ -0,0 +1,10 @@
+package projects.trees.slicing_tree;
+
+/**
+ * Represents a vertical cut in the SlicingTree
+ */
+public class VerticalCutSlice extends CutSlice {
+    public VerticalCutSlice() {
+        super(Cut.VERTICAL);
+    }
+}
