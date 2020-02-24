f75775e896c42994b67fb75de91a4c4e7312fae9
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-02-24 12:55:35 +0300

    Adds blank implementation for GameTree pickMove method.

--- src/projects/trees/game_tree/GameTree.java
+++ src/projects/trees/game_tree/GameTree.java
@@ -1,4 +1,21 @@
 package projects.trees.game_tree;
 
+import base.fundamental.TicTacToe;
+import base.trees.Position;
+
+/**
+ * A program that play Tic-Tac-Toe effectively. Constructs the entire game tree and picks the optimal move by using
+ * minimax evaluation function for performance
+ */
 public class GameTree {
+    /**
+     * Takes the current game state, finds and returns an optimal game stage for the current player
+     *
+     * @param gameState Current TicTacToe game state
+     * @return TicTacToe game state with the optimal move
+     * @throws IllegalArgumentException If the given game state is not a valid TicTacToe game
+     */
+    public Position<TicTacToe> pickMove(Position<TicTacToe> gameState) throws IllegalArgumentException {
+        return null;
+    }
 }
