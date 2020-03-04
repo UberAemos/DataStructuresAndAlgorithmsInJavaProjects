339d664672b131955ee9055a11b457bc8c67982f
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-03-04 11:49:37 +0300

    Adds blank drawTree method.

--- src/projects/trees/TreeLayout.java
+++ src/projects/trees/TreeLayout.java
@@ -1,7 +1,12 @@
 package projects.trees;
 
+import base.trees.Tree;
+
 /**
  * Includes certain methods to draw schematic tree representations.
  */
 public class TreeLayout {
+    public static <E> String drawBinaryTree(Tree<E> T) {
+        return null;
+    }
 }
