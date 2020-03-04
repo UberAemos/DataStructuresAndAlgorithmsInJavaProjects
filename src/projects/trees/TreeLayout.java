c0c1760f57a67a333cd52ab8a94726051be8946b
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-03-04 14:38:25 +0300

    Adds unit tested drawBinaryTree method.

--- src/projects/trees/TreeLayout.java
+++ src/projects/trees/TreeLayout.java
@@ -1,12 +1,38 @@
 package projects.trees;
 
-import base.trees.Tree;
+import base.trees.LinkedBinaryTree;
+import base.trees.Position;
+
+import java.util.Iterator;
 
 /**
  * Includes certain methods to draw schematic tree representations.
  */
 public class TreeLayout {
-    public static <E> String drawBinaryTree(Tree<E> T) {
-        return null;
+
+    /**
+     * Draws a binary tree in a schematic format
+     *
+     * @param T   Binary tree to be drawn
+     * @param <E>
+     * @return Schematic tree representation in String format
+     */
+    public static <E> String drawBinaryTree(LinkedBinaryTree<E> T) {
+        return drawBinaryNode(T, T.root(), new StringBuilder(), 0);
+    }
+
+    private static <E> String drawBinaryNode(LinkedBinaryTree<E> t, Position<E> position, StringBuilder sb, int depth) {
+        for (int i = 1; i < depth; i++) {
+            sb.append("|  ");
+        }
+        if (depth != 0) sb.append("|--");
+        sb.append(position.getElement());
+
+        Iterator<Position<E>> iterator = t.children(position).iterator();
+        while (iterator.hasNext()) {
+            sb.append("\n");
+            drawBinaryNode(t, iterator.next(), sb, depth + 1);
+        }
+        return sb.toString();
     }
 }
