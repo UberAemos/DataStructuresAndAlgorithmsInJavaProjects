ab20a4ca02374d90e8f76c8b7655b1a72461bd16
Anıl Sakaryalı <aanilsakaryali@gmail.com>
2020-03-04 15:15:49 +0300

    Fixes unit tests for General Tree coverage.

--- src/projects/trees/TreeLayoutTest.java
+++ src/projects/trees/TreeLayoutTest.java
@@ -1,54 +1,106 @@
 package projects.trees;
 
 import base.trees.LinkedBinaryTree;
+import base.trees.Position;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TreeLayoutTest {
 
-    LinkedBinaryTree<Integer> oneNodeTree;
-    LinkedBinaryTree<Integer> simpleTree;
-    LinkedBinaryTree<Integer> complexTree;
+    LinkedBinaryTree<Integer> oneNodeBinaryTree;
+    LinkedBinaryTree<Integer> simpleBinaryTree;
+    LinkedBinaryTree<Integer> complexBinaryTree;
+
+    LinkedTree<Integer> oneNodeTree;
+    LinkedTree<Integer> simpleTree;
+    LinkedTree<Integer> complexTree;
+
+    private String oneNodeBinaryTreeRepresentation = "0";
+    private String simpleBinaryTreeRepresentation = "0\n|--1\n|--2";
+    private String complexBinaryTreeRepresentation = "0\n|--1\n|  |--3\n|--2\n|  |--4\n|  |--5\n|  |  |--6";
 
     private String oneNodeTreeRepresentation = "0";
-    private String simpleTreeRepresentation = "0\n|--1\n|--2";
-    private String complexTreeRepresentation = "0\n|--1\n|  |--3\n|--2\n|  |--4\n|  |--5\n|  |  |--6";
+    private String simpleTreeRepresentation = "0\n|--1\n|--2\n|--3";
+    private String complexTreeRepresentation = "0\n|--1\n|  |--4\n|  |--5\n|--2\n|  |--6\n|  |  |--7\n|--3";
 
     @BeforeEach
     void init() {
-        oneNodeTree = new LinkedBinaryTree<>();
+        oneNodeBinaryTree = new LinkedBinaryTree<>();
+        oneNodeBinaryTree.addRoot(0);
+
+        simpleBinaryTree = new LinkedBinaryTree<>();
+        simpleBinaryTree.addRoot(0);
+        simpleBinaryTree.addLeft(simpleBinaryTree.root(), 1);
+        simpleBinaryTree.addRight(simpleBinaryTree.root(), 2);
+
+        complexBinaryTree = new LinkedBinaryTree<>();
+        complexBinaryTree.addRoot(0);
+        complexBinaryTree.addLeft(complexBinaryTree.root(), 1);
+        complexBinaryTree.addRight(complexBinaryTree.root(), 2);
+        complexBinaryTree.addRight(complexBinaryTree.left(complexBinaryTree.root()), 3);
+        complexBinaryTree.addLeft(complexBinaryTree.right(complexBinaryTree.root()), 4);
+        complexBinaryTree.addRight(complexBinaryTree.right(complexBinaryTree.root()), 5);
+        complexBinaryTree.addRight(complexBinaryTree.right(complexBinaryTree.right(complexBinaryTree.root())), 6);
+
+        oneNodeTree = new LinkedTree<>();
         oneNodeTree.addRoot(0);
 
-        simpleTree = new LinkedBinaryTree<>();
+        simpleTree = new LinkedTree<>();
         simpleTree.addRoot(0);
-        simpleTree.addLeft(simpleTree.root(), 1);
-        simpleTree.addRight(simpleTree.root(), 2);
+        simpleTree.addChild(1, simpleTree.root);
+        simpleTree.addChild(2, simpleTree.root);
+        simpleTree.addChild(3, simpleTree.root);
 
-        complexTree = new LinkedBinaryTree<>();
+        complexTree = new LinkedTree<>();
         complexTree.addRoot(0);
-        complexTree.addLeft(complexTree.root(), 1);
-        complexTree.addRight(complexTree.root(), 2);
-        complexTree.addRight(complexTree.left(complexTree.root()), 3);
-        complexTree.addLeft(complexTree.right(complexTree.root()), 4);
-        complexTree.addRight(complexTree.right(complexTree.root()), 5);
-        complexTree.addRight(complexTree.right(complexTree.right(complexTree.root())), 6);
+        complexTree.addChild(1, complexTree.root);
+        complexTree.addChild(2, complexTree.root);
+        complexTree.addChild(3, complexTree.root);
+
+        Iterator<Position<Integer>> iterator = complexTree.children(complexTree.root).iterator();
+        Position<Integer> next = iterator.next();
+        complexTree.addChild(4, next);
+        complexTree.addChild(5, next);
+        next = iterator.next();
+        complexTree.addChild(6, next);
+
+        iterator = complexTree.children(next).iterator();
+        next = iterator.next();
+        complexTree.addChild(7, next);
+    }
+
+    @Test
+    void whenOneNodeBinaryTreeIsDrawn_thenReturnsOneNodeBinaryTreeRepresentation() {
+        assertEquals(oneNodeBinaryTreeRepresentation, TreeLayout.drawTree(oneNodeBinaryTree));
+    }
+
+    @Test
+    void whenSimpleBinaryTreeIsDrawn_thenReturnsOneNodeBinaryTreeRepresentation() {
+        assertEquals(simpleBinaryTreeRepresentation, TreeLayout.drawTree(simpleBinaryTree));
+    }
+
+    @Test
+    void whenComplexBinaryTreeIsDrawn_thenReturnsOneNodeBinaryTreeRepresentation() {
+        assertEquals(complexBinaryTreeRepresentation, TreeLayout.drawTree(complexBinaryTree));
     }
 
     @Test
     void whenOneNodeTreeIsDrawn_thenReturnsOneNodeTreeRepresentation() {
-        assertEquals(oneNodeTreeRepresentation, TreeLayout.drawBinaryTree(oneNodeTree));
+        assertEquals(oneNodeTreeRepresentation, TreeLayout.drawTree(oneNodeTree));
     }
 
     @Test
     void whenSimpleTreeIsDrawn_thenReturnsOneNodeTreeRepresentation() {
-        assertEquals(simpleTreeRepresentation, TreeLayout.drawBinaryTree(simpleTree));
+        assertEquals(simpleTreeRepresentation, TreeLayout.drawTree(simpleTree));
     }
 
     @Test
     void whenComplexTreeIsDrawn_thenReturnsOneNodeTreeRepresentation() {
-        assertEquals(complexTreeRepresentation, TreeLayout.drawBinaryTree(complexTree));
+        assertEquals(complexTreeRepresentation, TreeLayout.drawTree(complexTree));
     }
 
 }
